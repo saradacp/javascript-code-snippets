8bfc7514cb4c338a21165ca67c0457a7ac116092
Sarada Cherukupalli <scherukupalli@walmartlabs.com>
2016-11-15 08:29:10 -0800

    function overloading in js

---
+++ function_overloading.js
@@ -0,0 +1,19 @@
+function greet(firstname, lastname, language) {
+  var language = language || 'en';
+  if (language === 'en') {
+    console.log("Hello "+firstname +" " +lastname);
+  }
+  if (language === 'es') {
+    console.log("Hola "+ firstname +" " +lastname);
+  }
+}
+function greetEnglish(firstname, lastname){
+  greet(firstname, lastname,'en');
+}
+function greetSpanish(firstname, lastname){
+  greet(firstname, lastname,'es');
+}
+greetEnglish('John','Doe');
+greetSpanish('John','Doe');
+
+
